167443c41a9c36686700749e6b6e9381d0fe15d2
Glenn Gallano <ggallano@live.com>
2025-03-07 13:28:15 +0800

    Added TextExtractor Class

--- AdeventOfCode/AoC_5/Program.cs
+++ AdeventOfCode/AoC_5/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace AoC_5
+﻿using System.Text.RegularExpressions;
+using Utilities;
+
+namespace AoC_5
 {
     internal class Program
     {
+        private const string _inputPath = @"C:\Users\ggallan2\Documents\Project_Code\_Github\AdventOfCOde\AdeventOfCode\AoC_5\input\input.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var inputText = InputFileReader.ReadText(_inputPath);
+            MatchCollection matches = TextExtractor.ExtractText(inputText, @"\d*\|\d*");
         }
     }
 }
---
+++ AdeventOfCode/Utilities/TextExtractor.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Utilities
+{
+    public class TextExtractor
+    {
+
+        public static MatchCollection ExtractText(string inputText, string regPattern)
+        {
+            return Regex.Matches(inputText, regPattern);
+        }
+    }
+}
