1f3ee4d73516123d0ca4b6cee12a710fbd7d427b
Glenn Gallano <ggallano@live.com>
2025-06-13 17:09:48 +0800

    AoC_11 added AoC_7 AoC_6

---
+++ AdeventOfCode/AoC_11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Utilities;
+
+namespace AoC_11
+{
+    internal class Program
+    {
+        private const string _inputPath = @".\input\ex_input.txt";
+        private const string RegPattern = @"\d+";
+
+        static List<int> stones = new List<int>();
+        static List<List<int>> blinks = new List<List<int>>();
+
+        static void Main(string[] args)
+        {
+            var inputText = InputFileReader.ReadText(_inputPath);
+            MatchCollection matches = TextExtractor.ExtractText(inputText, RegPattern);
+
+            stones.AddRange(matches.Select(m => int.Parse(m.Value)).ToList());
+            blinks.Add(stones);
+
+            for (int i = 0; i < 25; i++)
+            {
+                var input = blinks.Last();
+                stones = new List<int>();
+
+                for (int j = 0; j < input.Count; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ AdeventOfCode/AoC_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using Utilities;
+
+namespace AoC_7
+{
+    internal class Program
+    {
+        private const string _inputPath = @".\input\input1.txt";
+        private const string RegPattern = @"\d+";
+        //  \d+(?=\:)
+
+        // (?<=\s)\d+
+
+        static List<Rope> Ropes = new List<Rope>();
+
+        static void Main(string[] args)
+        {
+            var inputText = InputFileReader.ReadText(_inputPath);
+            MatchCollection matches = TextExtractor.ExtractText(inputText, RegPattern);
+
+            //for (int i = 0; i < matches.Count; i += 3)
+            //{
+            //    Ropes.Add();
+            //}
+        }
+    }
+
+    public class Rope
+    {
+        public int LeftValue { get; set; }
+        public List<int> RightValue { get; set; }
+    }
+}
